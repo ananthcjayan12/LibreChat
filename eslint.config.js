104f6918cc14cb54f8c26e92dac69fd2ffd20f12
Ruben Talstra <RubenTalstra1211@outlook.com>
2025-02-08 17:44:01 +0100

    added import sort to linter

--- eslint.config.js
+++ eslint.config.js
@@ -20,258 +20,287 @@ const compat = new FlatCompat({
   allConfig: js.configs.all,
 });
 
-export default [{
-  ignores: [
-    'client/dist/**/*',
-    'client/public/**/*',
-    'e2e/playwright-report/**/*',
-    'packages/mcp/types/**/*',
-    'packages/mcp/dist/**/*',
-    'packages/mcp/test_bundle/**/*',
-    'api/demo/**/*',
-    'packages/data-provider/types/**/*',
-    'packages/data-provider/dist/**/*',
-    'packages/data-provider/test_bundle/**/*',
-    'data-node/**/*',
-    'meili_data/**/*',
-    '**/node_modules/**/*',
-  ],
-}, ...fixupConfigRules(compat.extends(
-  'eslint:recommended',
-  'plugin:react/recommended',
-  'plugin:react-hooks/recommended',
-  'plugin:jest/recommended',
-  'prettier',
-  'plugin:jsx-a11y/recommended',
-)), {
-  plugins: {
-    react: fixupPluginRules(react),
-    'react-hooks': fixupPluginRules(reactHooks),
-    '@typescript-eslint': typescriptEslintEslintPlugin,
-    import: importPlugin,
-    'jsx-a11y': fixupPluginRules(jsxA11Y),
-    'import/parsers': tsParser,
+export default [
+  {
+    ignores: [
+      'client/dist/**/*',
+      'client/public/**/*',
+      'e2e/playwright-report/**/*',
+      'packages/mcp/types/**/*',
+      'packages/mcp/dist/**/*',
+      'packages/mcp/test_bundle/**/*',
+      'api/demo/**/*',
+      'packages/data-provider/types/**/*',
+      'packages/data-provider/dist/**/*',
+      'packages/data-provider/test_bundle/**/*',
+      'data-node/**/*',
+      'meili_data/**/*',
+      '**/node_modules/**/*',
+    ],
   },
-
-  languageOptions: {
-    globals: {
-      ...globals.browser,
-      ...globals.node,
-      ...globals.commonjs,
+  ...fixupConfigRules(
+    compat.extends(
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:jest/recommended',
+      'prettier',
+      'plugin:jsx-a11y/recommended',
+    ),
+  ),
+  {
+    plugins: {
+      react: fixupPluginRules(react),
+      'react-hooks': fixupPluginRules(reactHooks),
+      '@typescript-eslint': typescriptEslintEslintPlugin,
+      import: importPlugin,
+      'jsx-a11y': fixupPluginRules(jsxA11Y),
+      'import/parsers': tsParser,
     },
 
-    parser: tsParser,
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.commonjs,
+      },
 
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    settings: {
+      react: {
+        createClass: 'createReactClass',
+        pragma: 'React',
+        fragment: 'Fragment',
+        version: 'detect',
+      },
+
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
+      },
+
+      'import/resolver': {
+        typescript: {
+          project: ['./client/tsconfig.json'],
+        },
+
+        node: {
+          project: ['./client/tsconfig.json'],
+        },
+      },
+    },
+
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+
+      '@typescript-eslint/ban-ts-comment': [
+        'error',
+        {
+          'ts-ignore': false,
+        },
+      ],
+
+      indent: ['error', 2, { SwitchCase: 1 }],
+
+      'max-len': [
+        'error',
+        {
+          code: 120,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreComments: true,
+        },
+      ],
+
+      'linebreak-style': 0,
+      curly: ['error', 'all'],
+      // Updated semi rule:
+      semi: ['error', 'always'],
+      'object-curly-spacing': ['error', 'always'],
+
+      'no-multiple-empty-lines': [
+        'error',
+        {
+          max: 1,
+        },
+      ],
+
+      'no-trailing-spaces': 'error',
+      'comma-dangle': ['error', 'always-multiline'],
+      'no-console': 'off',
+      'import/no-cycle': 'error',
+      'import/no-self-import': 'error',
+      'import/extensions': 'off',
+      'no-promise-executor-return': 'off',
+      'no-param-reassign': 'off',
+      'no-continue': 'off',
+      'no-restricted-syntax': 'off',
+      'react/prop-types': 'off',
+      'react/display-name': 'off',
+      'no-nested-ternary': 'error',
+
+      'no-unused-vars': [
+        'error',
+        {
+          varsIgnorePattern: '^_',
+        },
+      ],
+
+      quotes: ['error', 'single'],
+      'key-spacing': ['error', { beforeColon: false, afterColon: true }],
+
+      // Added sort-imports rule:
+      'sort-imports': [
+        'error',
+        {
+          ignoreCase: false,
+          ignoreDeclarationSort: false,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+          allowSeparatedGroups: false,
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      'no-unused-vars': 'off',
+      'react/display-name': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+  {
+    files: ['**/rollup.config.js', '**/.eslintrc.js', '**/jest.config.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
       },
     },
   },
-
-  settings: {
-    react: {
-      createClass: 'createReactClass',
-      pragma: 'React',
-      fragment: 'Fragment',
-      version: 'detect',
-    },
-
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-
-    'import/resolver': {
-      typescript: {
-        project: ['./client/tsconfig.json'],
-      },
-
-      node: {
-        project: ['./client/tsconfig.json'],
+  {
+    files: [
+      '**/*.test.js',
+      '**/*.test.jsx',
+      '**/*.test.ts',
+      '**/*.test.tsx',
+      '**/*.spec.js',
+      '**/*.spec.jsx',
+      '**/*.spec.ts',
+      '**/*.spec.tsx',
+      '**/setupTests.js',
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+        ...globals.node,
       },
     },
-  },
-
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-
-    '@typescript-eslint/ban-ts-comment': ['error', {
-      'ts-ignore': false,
-    }],
-
-    indent: ['error', 2, {
-      SwitchCase: 1,
-    }],
-
-    'max-len': ['error', {
-      code: 120,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: true,
-      ignoreComments: true,
-    }],
-
-    'linebreak-style': 0,
-    curly: ['error', 'all'],
-    semi: ['error', 'always'],
-    'object-curly-spacing': ['error', 'always'],
-
-    'no-multiple-empty-lines': ['error', {
-      max: 1,
-    }],
-
-    'no-trailing-spaces': 'error',
-    'comma-dangle': ['error', 'always-multiline'],
-    'no-console': 'off',
-    'import/no-cycle': 'error',
-    'import/no-self-import': 'error',
-    'import/extensions': 'off',
-    'no-promise-executor-return': 'off',
-    'no-param-reassign': 'off',
-    'no-continue': 'off',
-    'no-restricted-syntax': 'off',
-    'react/prop-types': ['off'],
-    'react/display-name': ['off'],
-    'no-nested-ternary': 'error',
-
-    'no-unused-vars': ['error', {
-      varsIgnorePattern: '^_',
-    }],
-
-    quotes: ['error', 'single'],
-    'key-spacing': ['error', { beforeColon: false, afterColon: true }],
-  },
-}, {
-  files: ['**/*.ts', '**/*.tsx'],
-
-  rules: {
-    'no-unused-vars': 'off',
-    'react/display-name': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
-  },
-}, {
-  files: ['**/rollup.config.js', '**/.eslintrc.js', '**/jest.config.js'],
-
-  languageOptions: {
-    globals: {
-      ...globals.node,
+    rules: {
+      'react/display-name': 'off',
+      'react/prop-types': 'off',
+      'react/no-unescaped-entities': 'off',
     },
   },
-}, {
-  files: [
-    '**/*.test.js',
-    '**/*.test.jsx',
-    '**/*.test.ts',
-    '**/*.test.tsx',
-    '**/*.spec.js',
-    '**/*.spec.jsx',
-    '**/*.spec.ts',
-    '**/*.spec.tsx',
-    '**/setupTests.js',
-  ],
+  ...compat
+    .extends(
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+    )
+    .map(config => ({
+      ...config,
+      files: ['**/*.ts', '**/*.tsx'],
+    })),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
 
-  languageOptions: {
-    globals: {
-      ...globals.jest,
-      ...globals.node,
+    plugins: {
+      '@typescript-eslint': typescriptEslintEslintPlugin,
+      jest: fixupPluginRules(jest),
+    },
+
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'script',
+
+      parserOptions: {
+        project: './client/tsconfig.json',
+      },
+    },
+
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unnecessary-condition': 'warn',
+      '@typescript-eslint/strict-boolean-expressions': 'warn',
     },
   },
-
-  rules: {
-    'react/display-name': 'off',
-    'react/prop-types': 'off',
-    'react/no-unescaped-entities': 'off',
-  },
-}, ...compat.extends(
-  'plugin:@typescript-eslint/eslint-recommended',
-  'plugin:@typescript-eslint/recommended',
-).map(config => ({
-  ...config,
-  files: ['**/*.ts', '**/*.tsx'],
-})), {
-  files: ['**/*.ts', '**/*.tsx'],
-
-  plugins: {
-    '@typescript-eslint': typescriptEslintEslintPlugin,
-    jest: fixupPluginRules(jest),
-  },
-
-  languageOptions: {
-    parser: tsParser,
-    ecmaVersion: 5,
-    sourceType: 'script',
-
-    parserOptions: {
-      project: './client/tsconfig.json',
+  {
+    // **Data-provider specific configuration block**
+    files: ['./packages/data-provider/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        project: './packages/data-provider/tsconfig.json',
+      },
     },
   },
-
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/no-unnecessary-condition': 'warn',
-    '@typescript-eslint/strict-boolean-expressions': 'warn',
+  {
+    files: ['./api/demo/**/*.ts'],
   },
-},
-{
-  // **Data-provider specific configuration block**
-  files: ['./packages/data-provider/**/*.ts'],
-  languageOptions: {
-    parser: tsParser,
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    parserOptions: {
-      project: './packages/data-provider/tsconfig.json',
+  {
+    files: ['./packages/mcp/**/*.ts'],
+  },
+  {
+    files: ['./config/translations/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'script',
+      parserOptions: {
+        project: './config/translations/tsconfig.json',
+      },
     },
   },
-},
-{
-  files: ['./api/demo/**/*.ts'],
-}, {
-  files: ['./packages/mcp/**/*.ts'],
-}, {
-  files: ['./config/translations/**/*.ts'],
-
-  languageOptions: {
-    parser: tsParser,
-    ecmaVersion: 5,
-    sourceType: 'script',
-
-    parserOptions: {
-      project: './config/translations/tsconfig.json',
+  {
+    files: ['./packages/data-provider/specs/**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 5,
+      sourceType: 'script',
+      parserOptions: {
+        project: './packages/data-provider/tsconfig.spec.json',
+      },
     },
   },
-}, {
-  files: ['./packages/data-provider/specs/**/*.ts'],
-
-  languageOptions: {
-    ecmaVersion: 5,
-    sourceType: 'script',
-
-    parserOptions: {
-      project: './packages/data-provider/tsconfig.spec.json',
+  {
+    files: ['./api/demo/specs/**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 5,
+      sourceType: 'script',
+      parserOptions: {
+        project: './packages/data-provider/tsconfig.spec.json',
+      },
     },
   },
-}, {
-  files: ['./api/demo/specs/**/*.ts'],
-
-  languageOptions: {
-    ecmaVersion: 5,
-    sourceType: 'script',
-
-    parserOptions: {
-      project: './packages/data-provider/tsconfig.spec.json',
+  {
+    files: ['./packages/mcp/specs/**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 5,
+      sourceType: 'script',
+      parserOptions: {
+        project: './packages/mcp/tsconfig.spec.json',
+      },
     },
   },
-}, {
-  files: ['./packages/mcp/specs/**/*.ts'],
-
-  languageOptions: {
-    ecmaVersion: 5,
-    sourceType: 'script',
-
-    parserOptions: {
-      project: './packages/mcp/tsconfig.spec.json',
-    },
-  },
-}];
+];
